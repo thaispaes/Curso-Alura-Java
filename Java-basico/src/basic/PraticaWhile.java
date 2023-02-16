6e02c5def69a4fe57b94e4864321297c6873e175
thaispaes <thaisreginapaesdossantos@gmail.com>
2023-02-16 19:35:36 -0300

    pratica de while e for

---
+++ Java-basico/src/basic/PraticaWhile.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class PraticaWhile {
+    public static void main(String[] args) {
+
+        int contador = 1;
+
+        while(contador <= 10) {
+
+            System.out.println(contador);
+            contador++;
+        }
+    }
+}
